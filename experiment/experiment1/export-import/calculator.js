64f12ea998b7b34260b2a5f5f815a7a437ac9e1b
kaushlendra singh <thakurkaushlendra32@gmail.com>
2026-02-18 12:32:31 +0530

    add a new file

--- experiment/experiment1/export-import/calculator.js
+++
@@ -1,6 +0,0 @@
-export function sum(a, b) {
-    return a + b;
-}
-export function subtract(a, b) {
-    return a - b;
-}
--- experiment/experiment1/export-import/index.js
+++
@@ -1,4 +0,0 @@
-const k = require("./calculator.js")
-
-console.log(k.sum(2,3));
-console.log(k.subtract(5,2));
